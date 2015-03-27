b15c93ad8e3e05e4ad6f0419bf725cdf8601597b
Albert Wang <albert@cellabus.com>
2015-03-26 22:16:15 -0700

    Convert tests from old 'should' to new 'expect' syntax

--- spec/apns/core_spec.rb
+++ spec/apns/core_spec.rb
@@ -9,20 +9,20 @@ describe APNS do
     end
     it "can read pem_content variable directly" do
       APNS.pem_content = 'asdf'
-      APNS.pem_content.should eq 'asdf'
+      expect(APNS.pem_content).to eq 'asdf'
     end
     it "can read pem_content from a file" do
       APNS.pem = 'file'
       expect(APNS).to receive(:read_pem).and_return('qwer')
-      APNS.pem_content.should eq 'qwer'
+      expect(APNS.pem_content).to eq 'qwer'
     end
     it "can read pem_content from a Proc" do
       APNS.pem = lambda { return Random.rand }
       value1 = APNS.pem_content
       value2 = APNS.pem_content
-      value1.should be_between(0, 1)
-      value2.should be_between(0, 1)
-      value1.should_not eq value2
+      expect(value1).to be_between(0, 1)
+      expect(value2).to be_between(0, 1)
+      expect(value1).not_to eq value2
     end
   end
 
