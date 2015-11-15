35fecea756a81c3d0ef545619194d773df531fe4
Albert Wang <albert@cellabus.com>
2015-11-14 21:17:59 -0800

    Run rubocop auto-correct

--- lib/apns/notification.rb
+++ lib/apns/notification.rb
@@ -1,11 +1,11 @@
 module APNS
-  require 'openssl'
+  require "openssl"
 
   class Notification
     attr_accessor :device_token, :alert, :badge, :sound, :other, :priority
     attr_accessor :message_identifier, :expiration_date
     attr_accessor :content_available
-    
+
     def initialize(device_token, message)
       self.device_token = device_token
       if message.is_a?(Hash)
@@ -16,59 +16,59 @@ module APNS
         self.message_identifier = message[:message_identifier]
         self.content_available = !message[:content_available].nil?
         self.expiration_date = message[:expiration_date]
-        self.priority = if self.content_available
-          message[:priority] || 5
-        else
-          message[:priority] || 10
+        self.priority = if content_available
+                          message[:priority] || 5
+                        else
+                          message[:priority] || 10
         end
       elsif message.is_a?(String)
         self.alert = message
       else
-        raise "Notification needs to have either a hash or string"
+        fail "Notification needs to have either a hash or string"
       end
 
       self.message_identifier ||= OpenSSL::Random.random_bytes(4)
     end
-        
+
     def packaged_notification
-      pt = self.packaged_token
-      pm = self.packaged_message
+      pt = packaged_token
+      pm = packaged_message
       pi = self.message_identifier
-      pe = (self.expiration_date || 0).to_i
-      pr = (self.priority || 10).to_i
+      pe = (expiration_date || 0).to_i
+      pr = (priority || 10).to_i
 
       # Each item consist of
       # 1. unsigned char [1 byte] is the item (type) number according to Apple's docs
       # 2. short [big endian, 2 byte] is the size of this item
       # 3. item data, depending on the type fixed or variable length
-      data = ''
+      data = ""
       data << [1, pt.bytesize, pt].pack("CnA*")
       data << [2, pm.bytesize, pm].pack("CnA*")
       data << [3, pi.bytesize, pi].pack("CnA*")
       data << [4, 4, pe].pack("CnN")
       data << [5, 1, pr].pack("CnC")
-      
+
       # Return the full notification frame:
       # Each notification frame consists of
       # 1. (e.g. protocol version) 2 (unsigned char [1 byte])
       # 2. size of the full frame (unsigend int [4 byte], big endian)
       # 3. the data assembled above
-      ([2, data.bytesize].pack('CN') + data)
+      ([2, data.bytesize].pack("CN") + data)
     end
-  
-    def packaged_token
-      [device_token.gsub(/[\s|<|>]/,'')].pack('H*')
-    end
-  
-    def packaged_message
-      aps = {'aps'=> {} }
-      aps['aps']['alert'] = self.alert if self.alert
-      aps['aps']['badge'] = self.badge if self.badge
-      aps['aps']['sound'] = self.sound if self.sound
-      aps['aps']['content-available'] = 1 if self.content_available
 
-      aps.merge!(self.other) if self.other
+    def packaged_token
+      [device_token.gsub(/[\s|<|>]/, "")].pack("H*")
+    end
+
+    def packaged_message
+      aps = { "aps" => {} }
+      aps["aps"]["alert"] = alert if alert
+      aps["aps"]["badge"] = badge if badge
+      aps["aps"]["sound"] = sound if sound
+      aps["aps"]["content-available"] = 1 if content_available
+
+      aps.merge!(other) if other
       aps.to_json
-    end    
+    end
   end
 end
--- spec/apns/notification_spec.rb
+++ spec/apns/notification_spec.rb
@@ -1,56 +1,52 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + "/../spec_helper"
 
 describe APNS::Notification do
-  
   it "should take a string as the message" do
-    n = APNS::Notification.new('device_token', 'Hello')
-    expect(n.alert).to eq('Hello')
+    n = APNS::Notification.new("device_token", "Hello")
+    expect(n.alert).to eq("Hello")
   end
-  
+
   it "should take a hash as the message" do
-    n = APNS::Notification.new('device_token', {:alert => 'Hello iPhone', :badge => 3})
+    n = APNS::Notification.new("device_token", { alert: "Hello iPhone", badge: 3 })
     expect(n.alert).to eq("Hello iPhone")
     expect(n.badge).to eq(3)
   end
-  
-  it "should have a priority if content_availible is set"  do
-    n = APNS::Notification.new('device_token', {:content_available => true})
+
+  it "should have a priority if content_availible is set" do
+    n = APNS::Notification.new("device_token", { content_available: true })
     expect(n.content_available).to be_truthy
     expect(n.priority).to eql(5)
   end
 
   describe '#packaged_message' do
-    
     it "should return JSON with notification information" do
-      n = APNS::Notification.new('device_token', {:alert => 'Hello iPhone', :badge => 3, :sound => 'awesome.caf'})
-      expect(n.packaged_message).to  eq("{\"aps\":{\"alert\":\"Hello iPhone\",\"badge\":3,\"sound\":\"awesome.caf\"}}")
+      n = APNS::Notification.new("device_token", { alert: "Hello iPhone", badge: 3, sound: "awesome.caf" })
+      expect(n.packaged_message).to eq("{\"aps\":{\"alert\":\"Hello iPhone\",\"badge\":3,\"sound\":\"awesome.caf\"}}")
     end
-    
+
     it "should not include keys that are empty in the JSON" do
-      n = APNS::Notification.new('device_token', {:badge => 3})
+      n = APNS::Notification.new("device_token", { badge: 3 })
       expect(n.packaged_message).to eq("{\"aps\":{\"badge\":3}}")
     end
 
     it "should return JSON with content availible" do
-      n = APNS::Notification.new('device_token', {:content_available => true})
-      expect(n.packaged_message).to  eq("{\"aps\":{\"content-available\":1}}")
+      n = APNS::Notification.new("device_token", { content_available: true })
+      expect(n.packaged_message).to eq("{\"aps\":{\"content-available\":1}}")
     end
-    
   end
-  
+
   describe '#package_token' do
     it "should package the token" do
-      n = APNS::Notification.new('<5b51030d d5bad758 fbad5004 bad35c31 e4e0f550 f77f20d4 f737bf8d 3d5524c6>', 'a')
+      n = APNS::Notification.new("<5b51030d d5bad758 fbad5004 bad35c31 e4e0f550 f77f20d4 f737bf8d 3d5524c6>", "a")
       expect(Base64.encode64(n.packaged_token)).to eq("W1EDDdW611j7rVAEutNcMeTg9VD3fyDU9ze/jT1VJMY=\n")
     end
   end
 
   describe '#packaged_notification' do
     it "should package the token" do
-      n = APNS::Notification.new('device_token', {:alert => 'Hello iPhone', :badge => 3, :sound => 'awesome.caf'})
-      expect(n).to receive(:message_identifier).and_return('aaaa') # make sure the message_identifier is not random
+      n = APNS::Notification.new("device_token", { alert: "Hello iPhone", badge: 3, sound: "awesome.caf" })
+      expect(n).to receive(:message_identifier).and_return("aaaa") # make sure the message_identifier is not random
       expect(Base64.encode64(n.packaged_notification)).to eq("AgAAAF4BAAbe8s79hOcCAEB7ImFwcyI6eyJhbGVydCI6IkhlbGxvIGlQaG9u\nZSIsImJhZGdlIjozLCJzb3VuZCI6ImF3ZXNvbWUuY2FmIn19AwAEYWFhYQQA\nBAAAAAAFAAEK\n")
     end
   end
-  
 end
